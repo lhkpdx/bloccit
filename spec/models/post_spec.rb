57bc9ee4c462cef4b34ad88041a2ed1800d360f8
Laura Kibben <smartgirlpdx@gmail.com>
2016-11-26 16:00:16 -0800

    Lesson 30 assignment complete

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Post, type: :model do
    let(:description) { RandomData.random_paragraph }
    let(:title) { RandomData.random_sentence }
    let(:body) { RandomData.random_paragraph }
+
    let(:topic) { Topic.create!(name: name, description: description) }
    let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
    let(:post) { topic.posts.create!(title: title, body: body, user: user) }
@@ -76,9 +77,28 @@ RSpec.describe Post, type: :model do
          it "updates the rank when a down vote is created" do
            old_rank = post.rank
            post.votes.create!(value: -1)
-           
+
            expect(post.rank).to eq (old_rank - 1)
          end
+      end
+
+       describe "#create_vote" do
+          it "sets the post up_votes to 1" do
+
+            expect(post.up_votes).to eq(@up_votes)
+          end
+
+          it "calls #create_vote when a post is created" do
+            post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+
+            expect(post).to receive(:create_vote)
+            post.save
+          end
+
+          it "associates the vote with the owner of the post" do
+
+            expect(post.votes.first.user).to eq(post.user)
+          end
        end
    end
  end
